ebbd0a2cd6b5f65692d6fccdc5bddd3695a2a26b
Przemyslaw Kowalczyk <git@pkowalczyk.pl>
2017-02-15 17:13:35 +0100

    Rewritten main part of flags module

--- flags.py
+++ flags.py
@@ -5,47 +5,47 @@ from pathlib import Path
 from git import Repo
 
 class Flag:
-    # rewrite state as @property https://docs.python.org/3.6/library/functions.html#property
     def __init__(self, name):
         assert name == name.upper(), 'Only upper flags names are allowed'        
         self.name = name
         self.path = Path('.', '.' + name)
         assert not self.path.is_dir(), 'Flag file cannot be a directory'
-        # assert self.check_good_state(), 'Wrong Flag file starting state'
-    
-    def _get_state(self):
+        assert self.check_state(), 'Wrong Flag file starting state'
+
+    def _state(self):
+        'Gets state without checking it.'
         if self.path.is_file():
-            with self.path.open('r') as f:
-                return f.read()
+            return self.path.read_text()
         else:
             return None
-
-    def get_state(self):
-        state = self._get_state()
+    
+    @property
+    def state(self):
+        state = self._state()
         if state != None:
-            assert self.__class__._check_good_state(state), 'Wrong state'
+            assert self.__class__._check_state(state), 'Wrong state'
         return state
 
-    def set_state(self, state):
+    @state.setter
+    def state(self, state):
         if state == None:
             if self.path.is_file():
                 self.path.unlink()
         else:            
             state = str(state)
-            assert self.__class__._check_good_state(state), 'Wrong state'
-            with self.path.open('w') as f:
-                f.write(state)
+            assert self.__class__._check_state(state), 'Wrong state'
+            self.path.write_text(state)
     
-    def check_good_state(self):
-        state = self._get_state()
-        return state == None or self.__class__._check_good_state(state)
+    def check_state(self):
+        state = self._state()
+        return state == None or self.__class__._check_state(state)
 
     @classmethod
-    def _check_good_state(cls, state):
+    def _check_state(cls, state):
         '''
-        Checked before state assigment and reading.
-        Child-classes can iplement this method.
-        None is always good.
+        Checked before state setter and after getter.
+        Child-classes can implement this method.
+        None is always a good state.
         '''
         return True
 
@@ -55,7 +55,7 @@ class Flag:
 #         self.repo = repo
 
 #     @classmethod
-#     def _check_good_state(cls, state):
+#     def _check_state(cls, state):
 #         git
 
 
--- tests/test_flag.py
+++ tests/test_flag.py
@@ -3,11 +3,14 @@ import unittest
 # from flags import Flag
 from blog_pfp.flags import Flag
 
+def unlink_if_file(path):
+    if path.is_file():
+        path.unlink()
 
 class FlagDigit(Flag):
     @classmethod
-    def _check_good_state(cls, state):
-        print(str(state), flush=True)
+    def _check_state(cls, state):
+        # print(str(state), flush=True)
         return str.isdigit(state)
 
 class Test_Flag(unittest.TestCase):
@@ -21,31 +24,33 @@ class Test_Flag(unittest.TestCase):
         self.assertRaises(OSError, Flag, '***@$#%^&*(A_FDVFYJ3676VBBV')
 
     def test_set_state(self):
-        self.f.set_state('test_flag_content')
-        self.assertEqual(self.f.get_state(), 'test_flag_content')
+        self.f.state = 'test_flag_content'
+        self.assertEqual(self.f.state, 'test_flag_content')
 
     def test_set_state_None(self):
-        self.f.set_state('test_flag_content')
-        self.f.set_state(None)
-        self.assertEqual(self.f.get_state(), None)
+        self.f.state = 'test_flag_content'
+        self.f.state = None
+        self.assertEqual(self.f.state, None)
 
-    def test_check_good_state(self):
-        self.f.set_state('test_flag_content')
-        self.assertTrue(self.f.check_good_state())        
-        self.f.set_state(None)
-        self.assertTrue(self.f.check_good_state())
+    def test_check_state(self):
+        self.f.state = 'test_flag_content'
+        self.assertTrue(self.f.check_state())      
+        self.f.state = None
+        self.assertTrue(self.f.check_state())
 
-    def test_check_good_state_create_checker(self):
-        self.fd.set_state(None)
-        self.assertTrue(self.fd.check_good_state())        
-        self.fd.set_state(1)
-        self.assertEqual(self.fd.get_state(), '1')
-        self.assertTrue(self.fd.check_good_state())        
-        self.assertRaises(AssertionError, self.fd.set_state, 'asd')
+    def test_check_state_create_checker(self): 
+        self.fd.state = None
+        self.assertTrue(self.fd.check_state())        
+        self.fd.state = 1
+        self.assertEqual(self.fd.state, '1')
+        self.assertTrue(self.fd.check_state())        
+        self.assertRaises(AssertionError, setattr, self.fd , 'state', 'asd')
 
     def tearDown(self):
-        self.f.set_state(None)
-        self.fd.set_state(None)
+        self.f.state = None
+        unlink_if_file(self.f.path)
+        self.fd.state = None
+        unlink_if_file(self.fd.path)
 
 if __name__ == '__main__':
     unittest.main()
